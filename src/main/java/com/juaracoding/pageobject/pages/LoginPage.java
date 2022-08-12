1751387dc280fd4239148cf7989e7c0f07a37475
Koswara <koswara.c79@gmail.com>
2022-08-12 21:35:21 +0700

    First Commit

---
+++ src/main/java/com/juaracoding/pageobject/pages/LoginPage.java
@@ -0,0 +1,35 @@
+package com.juaracoding.pageobject.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.juaracoding.pageobject.drivers.DriverSingleton;
+
+public class LoginPage {
+
+	private WebDriver driver;
+	
+	public LoginPage() {
+		this.driver = DriverSingleton.getDriver();
+		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(xpath = "//*[@id='txtUsername']")
+	private WebElement username;
+	
+	@FindBy(xpath = "//*[@id='txtPassword']")
+	private WebElement password;
+	
+	@FindBy(xpath = "//*[@id='btnLogin']")
+	private WebElement btnLogin;
+	
+	//scenario
+	public void login(String username, String password) {
+		this.username.sendKeys(username); //step
+		this.password.sendKeys(password);
+		btnLogin.click();
+	}
+	
+}
